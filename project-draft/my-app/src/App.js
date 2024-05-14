4701f081d1f389632c173d29c4d1b21ecdad4eeb
anjals5 <anjals5@uw.edu>
2024-05-14 15:48:36 -0700

    component changes without images working

--- project-draft/my-app/src/App.js
+++ project-draft/my-app/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
+import Cover from './Cover';
+
 import Nav from './Nav';
 import Predictions from './Predictions';
 import Container from './Container';
+// import Calendar from './Calendar';
+import Reading from './Reading';
+import MoodCheck from './MoodCheck';
 
 const App = () => {
     return (
       <div className="app">
         <Nav />
      
-       
+        {/* <Cover/> */}
         <Predictions />
         <Container />
+        {/* <Calendar/> */}
+        {/* <Reading/> */}
+        <MoodCheck />
+
+
    
       </div>
     );
---
+++ project-draft/my-app/src/MoodCheck.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const MoodCheck = () => (
+    <section className="mood_check">
+        <header>
+            <h3>How are you feeling today?</h3>
+    </header>
+    <div className="mood_top">
+        <img src="img/a_emoji1.png" alt="happy face"/>
+        <img src="img/a_emoji2.png" alt="side eye face"/>
+        <img src="img/a_emoji3.png" alt="sobbing face"/>
+    </div>
+    <div className="mood_bottom">
+        <img src="img/a_emoji4.png" alt="angry face"/>
+        <img src="img/a_emoji5.png" alt="happy face"/>
+        <img src="img/a_emoji6.png" alt="side eye face"/>
+    </div>
+</section>
+);
+
+
+export default MoodCheck;
