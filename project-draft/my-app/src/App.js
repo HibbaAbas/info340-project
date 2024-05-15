f54a5a1927bc618ec0237055884409180939d78a
AjaxKronus <107383559+AjaxKronus@users.noreply.github.com>
2024-05-15 11:57:34 -0700

    Update App.js

--- project-draft/my-app/src/App.js
+++ project-draft/my-app/src/App.js
@@ -8,23 +8,35 @@ import Container from './Container';
 // import Calendar from './Calendar';
 import Reading from './Reading';
 import MoodCheck from './MoodCheck';
+// Amartya added-Start
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// END
 
 const App = () => {
-    return (
-      <div className="app">
-        <Nav />
-     
-        {/* <Cover/> */}
-        <Predictions />
-        <Container />
-        {/* <Calendar/> */}
-        {/* <Reading/> */}
-        <MoodCheck />
+  return (
+    <div className="app">
+      <Nav />
+      {/* Amartya Added this. Check if it is useful and if it is correct - START */}
+      <Switch>
+      <Route path="/" exact component={Cover} />
+          <Route path="/predictions" component={Predictions} />
+          <Route path="/container" component={Container} />
+          <Route path="/reading" component={Reading} />
+          <Route path="/mood-check" component={MoodCheck} />
+          {/* Add other routes as necessary */}
+      </Switch>
+      {/* END */}
+      {/* <Cover/> */}
+      <Predictions />
+      <Container />
+      {/* <Calendar/> */}
+      {/* <Reading/> */}
+      <MoodCheck />
 
 
-   
-      </div>
-    );
-  };
+  
+    </div>
+  );
+};
   
   export default App;
