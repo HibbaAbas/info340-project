15c11ffe41376e76a1fd478e209bdf06d28d9b7d
Hiba Abbas <hibaabbas@Hibas-MacBook-Pro.local>
2024-05-15 21:44:48 -0700

    working react app with no router

--- project-draft/my-app/src/App.js
+++ project-draft/my-app/src/App.js
@@ -1,50 +1,24 @@
-import React from 'react';
-
-import Cover from './Cover';
-
-import Nav from './Nav';
-import Predictions from './Predictions';
-import Container from './Container';
 // import Calendar from './Calendar';
 import Reading from './Reading';
 import MoodCheck from './MoodCheck';
-// Amartya added-Start
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// END
+import Nav from './Nav';
+import Predictions from './Predictions';
+import Container from './Container';
 
 const App = () => {
-  return (
-    <div className="app">
-      <Nav />
-      
-      {/* Amartya Added this. Check if it is useful and if it is correct - START */}
-      <Switch>
-        {/* Renders only the first matching Route based on URL path */}
-        <Route path="/" exact component={Cover} />
-        {/* Renders 'Cover' component for exact match of root path "/" */}
-        <Route path="/predictions" component={Predictions} />
-        {/* Renders 'Predictions' for "/predictions" path */}
-        <Route path="/container" component={Container} />
-        {/* Renders 'Container' for "/container" path */}
-        <Route path="/reading" component={Reading} />
-        {/* Renders 'Reading' for "/reading" path */}
-        <Route path="/mood-check" component={MoodCheck} />
-        {/* Renders 'MoodCheck' for "/mood-check" path */}
-        {/* Add other routes as necessary */}
-      </Switch>
-      {/* END */}
+    return (
+      <div className="app">
+        <Nav />
 
-      {/* <Cover/> */}
-      <Predictions />
-      <Container />
-      {/* <Calendar/> */}
-      {/* <Reading/> */}
-      <MoodCheck />
+        {/* <Cover/> */}
+        <Predictions />
+        <Container />
+        {/* <Calendar/> */}
+        {/* <Reading/> */}
+        <MoodCheck />
 
+      </div>
+    );
+  };
 
-  
-    </div>
-  );
-};
-  
   export default App;
