3b6714ab78fc65612c47cc9e9752b7aa529eac0a
anjals5 <anjals5@uw.edu>
2024-05-31 22:44:55 -0700

    commented out login errors

--- project-draft/my-app/src/components/Cover.js
+++ project-draft/my-app/src/components/Cover.js
@@ -5,30 +5,30 @@ import {useAuthState} from 'react-firebase-hooks/auth';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 
-const firebaseUIConfig = {
-    signInOptions: [
-        GoogleAuthProvider.PROVIDER_ID,
-        {provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName:true},
+// const firebaseUIConfig = {
+//     signInOptions: [
+//         GoogleAuthProvider.PROVIDER_ID,
+//         {provider: EmailAuthProvider.PROVIDER_ID, requiredDisplayName:true},
 
-    ],
-    signInFlow: 'popup',
-    credentialHelper: 'none',
-    callbacks: {
-        signInSuccessWithAuthResult: () => false,
-    },
-};
+//     ],
+//     signInFlow: 'popup',
+//     credentialHelper: 'none',
+//     callbacks: {
+//         signInSuccessWithAuthResult: () => false,
+//     },
+// };
 
 
 export function Cover(props) {
-    const auth = getAuth();
-    const [user, loading, error] = useAuthState(auth);
+    // const auth = getAuth();
+    // const [user, loading, error] = useAuthState(auth);
 
-    useEffect(() => {
-        if(user) {
-            console.log('User Logged In:', user.displayName);
+    // useEffect(() => {
+    //     if(user) {
+    //         console.log('User Logged In:', user.displayName);
 
-        }
-    }, [user]);
+    //     }
+    // }, [user]);
 
     return (
         <div className="cover">
@@ -37,18 +37,18 @@ export function Cover(props) {
                 <div className="log-in-form">
                     <h1>Health Harmony</h1>
                     <h2>Empowering your period</h2>
-                    <h3>{user ? `Welcome, ${user.displayName}` : 'Log in'}</h3>
+                    {/* <h3>{user ? `Welcome, ${user.displayName}` : 'Log in'}</h3>
                     {loading && <p>Loading...</p>}
                     {error && <p>Error: {error.message}</p>}
                     {!user && !loading && (
-                        <>
-                        <StyledFirebaseAuth uiConfig={firebaseUIConfig} firebaseAuth={auth}/>
+                        <> */}
+                        {/* <StyledFirebaseAuth uiConfig={firebaseUIConfig} firebaseAuth={auth}/> */}
                         <p>New User? Create an Account</p>
-                        </>
-                    )}
-                    {user && (
-                        <button onClick={() => auth.signOut()}>Log Out</button>
-                    )}
+                        {/* </>
+                    )} */}
+                    {/* {user && ( */}
+                        {/* <button onClick={() => auth.signOut()}>Log Out</button> */}
+                    {/* )} */}
                    
                 </div>
             </div> 
--- project-draft/my-app/src/index.js
+++ project-draft/my-app/src/index.js
@@ -9,22 +9,22 @@ import App from './components/App';
 
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBw3NOPo_sTjzdoflksL1MEkSPJ0S2PtRc",
-  authDomain: "info340group-5.firebaseapp.com",
-  projectId: "info340group-5",
-  storageBucket: "info340group-5.appspot.com",
-  messagingSenderId: "292450042823",
-  appId: "1:292450042823:web:db53547a3f5b6514670fe2"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBw3NOPo_sTjzdoflksL1MEkSPJ0S2PtRc",
+//   authDomain: "info340group-5.firebaseapp.com",
+//   projectId: "info340group-5",
+//   storageBucket: "info340group-5.appspot.com",
+//   messagingSenderId: "292450042823",
+//   appId: "1:292450042823:web:db53547a3f5b6514670fe2"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
